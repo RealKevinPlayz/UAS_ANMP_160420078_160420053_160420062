760953266367f29eda7891637a309a3dd97c54d8
RealKevinPlayz <kliemoa@gmail.com>
2023-04-07 16:52:08 +0700

    UTS ANMP Theme / Topics : Healthcare UMC Commits : 1 Changes Desc : 1. Project Created 2. Gradle Dependencies added & synced

---
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
