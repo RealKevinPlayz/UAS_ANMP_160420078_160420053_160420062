908ce2c999cd3e9520062758b44120dc1d6d7156
RealKevinPlayz <kliemoa@gmail.com>
2023-05-07 15:10:04 +0700

    UTS ANMP Theme / Topics : Healthcare UMC Commits : 11 Changes Desc : 1. Bottom Navigation View Added 2. NavigationView Added

--- app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/view/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/view/MainActivity.kt
@@ -2,11 +2,30 @@ package id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.fragmentHost)
+                as NavHostFragment).navController
+
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+
+        bottomNav.setupWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
+                || super.onSupportNavigateUp()
     }
 }
