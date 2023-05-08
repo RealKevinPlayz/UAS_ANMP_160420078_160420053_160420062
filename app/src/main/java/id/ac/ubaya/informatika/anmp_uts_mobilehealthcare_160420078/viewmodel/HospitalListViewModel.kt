eb0f444654651aa9dfebe8dfe26539a981b9b23d
RealKevinPlayz <kliemoa@gmail.com>
2023-05-08 21:28:14 +0700

    UTS ANMP Theme / Topics : Healthcare UMC Commits : 15 Changes Desc : 1. HomeFragment.kt added 2. home vector asset added 3. healthcare_navigation.xml updated 4. new app logo added but not displaying picture

--- app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/viewmodel/HospitalListViewModel.kt
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/viewmodel/HospitalListViewModel.kt
@@ -17,9 +17,22 @@ class HospitalListViewModel(application: Application): AndroidViewModel(applicat
         hospitalLoadErrorLD.value = false
 
         val hospitalsJson = "[{'hospitalId' : '1','hospitalName' : 'Rumah Sakit Mitra Keluarga Kenjeran'," +
-                "'hospitalWebsite' : 'https://www.mitrakeluarga.com/kenjeran/menu-fasilitas-rs'," +
+                "'hospitalWebsite' : 'www.mitrakeluarga.com/kenjeran/menu-fasilitas-rs'," +
                 "'hospitalAddress' : 'Jl. Raya Kenjeran 506, Surabaya'," +
                 "'hospitalPhone' : '0813-9787-1313','hospitalPhotoUrl' : 'https://lh3.googleusercontent.com/p/AF1QipOzC1O5ko6qJqEcZtYCboaL4FsmHL1o2irqPeG9=s680-w680-h510'," +
+                "'hospitalRating' : '4.5'}, {'hospitalId' : '2','hospitalName' : 'Memorial Hospital'," +
+                "'hospitalWebsite' : 'www.memorialhospital.com'," +
+                "'hospitalAddress' : '456 Oak St, Anytown, USA'," +
+                "'hospitalPhone' : '3421-2232-1990','hospitalPhotoUrl' : 'https://wearememorial.com/wp-content/uploads/2022/08/D75_7357-1-1200x607.jpg'," +
+                "'hospitalRating' : '4.2'}, {'hospitalId' : '3','hospitalName' : 'Mercy Hospital'," +
+                "'hospitalWebsite' : 'www.mercyhospital.com'," +
+                "'hospitalAddress' : '789 Elm St, Anytown, USA'," +
+                "'hospitalPhone' : '3425-9787-1121','hospitalPhotoUrl' : 'https://www.mercy.net/content/dam/mercy/en/images/Mercy_Hospital_NWA_night.jpg'," +
+                "'hospitalRating' : '3.8'}, " +
+                "{'hospitalId' : '4','hospitalName' : 'University Hospital'," +
+                "'hospitalWebsite' : 'www.universityhospital.com'," +
+                "'hospitalAddress' : '222 Pine St, Anytown, USA'," +
+                "'hospitalPhone' : '5551-3456-0990','hospitalPhotoUrl' : 'https://www.muhealth.org/sites/default/files/styles/slider_hero_full_width/public/2017-08/160412%20UHFlowersTulips-31-952_0.jpg?itok=dtRLxHxw'," +
                 "'hospitalRating' : '4.5'}]"
 
         val sType = object : TypeToken<ArrayList<Hospital>>() { }.type
