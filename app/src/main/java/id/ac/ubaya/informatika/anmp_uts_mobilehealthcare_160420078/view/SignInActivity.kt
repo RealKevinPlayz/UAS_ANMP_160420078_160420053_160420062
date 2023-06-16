390c3fea177e00f64a4d2456f0b254cea2138a6a
NatasyaTanjaya8 <120316343+NatasyaTanjaya8@users.noreply.github.com>
2023-06-16 17:58:29 +0700

    coba 2

---
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/view/SignInActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
