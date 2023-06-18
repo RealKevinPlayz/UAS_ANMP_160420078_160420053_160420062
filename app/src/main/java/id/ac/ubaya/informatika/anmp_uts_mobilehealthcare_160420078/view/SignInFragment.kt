08d4b020c3ffadeb404e2d2b72d0990ec1fe3a4b
NatasyaTanjaya8 <120316343+NatasyaTanjaya8@users.noreply.github.com>
2023-06-18 16:44:36 +0700

    2

--- app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/view/SignInFragment.kt
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/view/SignInFragment.kt
@@ -1,11 +1,14 @@
 package id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078.R
+import kotlinx.android.synthetic.main.fragment_sign_in.*
 
 class SignInFragment : Fragment() {
     override fun onCreateView(
@@ -16,4 +19,15 @@ class SignInFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonRegister.setOnClickListener {
+            val action = SignInFragmentDirections.signInToSignUp()
+            Navigation.findNavController(it).navigate(action)
+        }
+        buttonSignIn.setOnClickListener {
+            var intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/view/SignUpFragment.kt
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/view/SignUpFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078.R
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 class SignUpFragment : Fragment() {
     override fun onCreateView(
@@ -16,4 +18,11 @@ class SignUpFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnBack.setOnClickListener {
+            val action = SignUpFragmentDirections.signUpToSignIn()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
