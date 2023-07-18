9ceb95df6c53f0a4b7a2966ba9376177c10915ea
NatanaelGeraldi <s160420062@student.ubaya.ac.id>
2023-07-18 12:58:32 +0800

    Mengisi data hospital

--- app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/view/HospitalListFragment.kt
+++ app/src/main/java/id/ac/ubaya/informatika/anmp_uts_mobilehealthcare_160420078/view/HospitalListFragment.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078.R
+import id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078.model.Hospital
 import id.ac.ubaya.informatika.anmp_uts_mobilehealthcare_160420078.viewmodel.HospitalListViewModel
 import kotlinx.android.synthetic.main.fragment_doctors_list.*
 import kotlinx.android.synthetic.main.fragment_hospital_list.*
@@ -42,6 +43,16 @@ class HospitalListFragment : Fragment() {
         }
 
         observeViewModel()
+        /*
+        var hospital1 = Hospital("Rumah Sakit Mitra Keluarga Kenjeran", "www.mitrakeluarga.com/kenjeran/menu-fasilitas-rs", "Jl. Raya Kenjeran 506, Surabaya", "0813-9787-1313", "https://lh3.googleusercontent.com/p/AF1QipOzC1O5ko6qJqEcZtYCboaL4FsmHL1o2irqPeG9=s680-w680-h510", "4.5")
+        var hospital2 = Hospital("Memorial Hospital", "www.memorialhospital.com", "456 Oak St, Anytown, USA", "3421-2232-1990", "https://wearememorial.com/wp-content/uploads/2022/08/D75_7357-1-1200x607.jpg", "4.2")
+        var hospital3 = Hospital("Mercy Hospital", "www.mercyhospital.com", "789 Elm St, Anytown, USA", "3425-9787-1121", "https://www.mercy.net/content/dam/mercy/en/images/Mercy_Hospital_NWA_night.jpg", "3.8")
+        var hospital4 = Hospital("University Hospital", "www.universityhospital.com", "222 Pine St, Anytown, USA", "5551-3456-0990", "https://www.muhealth.org/sites/default/files/styles/slider_hero_full_width/public/2017-08/160412%20UHFlowersTulips-31-952_0.jpg?itok=dtRLxHxw", "4.5")
+        viewModel.addHospital(hospital1)
+        viewModel.addHospital(hospital2)
+        viewModel.addHospital(hospital3)
+        viewModel.addHospital(hospital4)
+        */
     }
 
     fun observeViewModel() {
